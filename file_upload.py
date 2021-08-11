311e246702b5a5cbedd4e7dd19cec6e249001450
Elilora Nerat <75105317+Elilora@users.noreply.github.com>
2021-08-11 16:08:32 +0100

    Update file_upload.py

--- file_upload.py
+++ file_upload.py
@@ -22,13 +22,12 @@ def main():
         if st.button("Process"):
             file_details = {"Filename":docx_file.name,"FileType":docx_file.type,"FileSize":docx_file.size}
             st.write(file_details)
-			# Check File Type
+	    # Check File Type
             if docx_file.type == "text/plain":
                     st.text(str(docx_file.read(),"utf-8")) 
                     raw_text = str(docx_file.read(),"utf-8") 
             elif docx_file.type == "application/pdf":
                    raw_text = read_pdf(docx_file)
-					# st.write(raw_text)
                    try:
                        with pdfplumber.open(docx_file) as pdf:
                            page = pdf.pages[0]
@@ -37,9 +36,8 @@ def main():
                                st.write("None")
 				
             elif docx_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-				# Use the right file processor ( Docx,Docx2Text,etc)
                     raw_text = docx2txt.process(docx_file) # Parse in the uploadFile Class directory
-                    #st.write(raw_text)
+                
     
     return raw_text 
 
